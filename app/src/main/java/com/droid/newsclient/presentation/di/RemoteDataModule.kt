a5799d0bd42d59d3d62347e2522cfe2c7484feb8
PeteHack <pwachira900@gmail.com>
2021-11-28 23:58:04 +0300

    update project dependencies and fix some bugs

--- app/src/main/java/com/droid/newsclient/presentation/di/RemoteDataModule.kt
+++ app/src/main/java/com/droid/newsclient/presentation/di/RemoteDataModule.kt
@@ -6,11 +6,11 @@ import com.droid.newsclient.data.repository.datasourceImpl.NewsDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 class RemoteDataModule {
     @Singleton
